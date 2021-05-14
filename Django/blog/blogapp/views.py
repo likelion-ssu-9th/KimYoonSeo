4ef35d4b0121a106d9a6185bcd5f266fd404605c
김윤서 <yo26on@soongsil.ac.kr>
2021-05-15 01:26:18 +0900

    [ADD] Django 과제업로드

---
+++ Django/blog/blogapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+# Create your views here.
+
+def detail(request,id):
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request,'detail.html',{'blog': blog})
+
+def home(request):
+    blogs = Blog.objects.all()
+    return render(request,'home.html',{'blogs':blogs})
+
+
