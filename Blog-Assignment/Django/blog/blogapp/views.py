37922460e0c50af26af12f251557d2b0251ea0cd
김윤서 <yo26on@soongsil.ac.kr>
2021-05-24 15:38:18 +0900

    [ADD] new, create 추가

--- Blog-Assignment/Django/blog/blogapp/views.py
+++ Blog-Assignment/Django/blog/blogapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
+from django.utils import timezone
 from .models import Blog
 # Create your views here.
 
@@ -10,4 +11,14 @@ def home(request):
     blogs = Blog.objects.all()
     return render(request,'home.html',{'blogs':blogs})
 
+def new(request):
+    return render(request,'new.html')
 
+def create(requset):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now
+    new_blog.save()
+    return redirect('detail', new_blog.id)
