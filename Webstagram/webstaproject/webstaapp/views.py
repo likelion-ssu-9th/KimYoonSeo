95fc4ec8b6b362805880e3169bf544b8a90d915c
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-06-16 15:32:43 +0900

    [FEAT] login 구현

--- Webstagram/webstaproject/webstaapp/views.py
+++ Webstagram/webstaproject/webstaapp/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Profile
 from django.http import HttpResponse
-
+from django.utils import timezone
+from .models import Feed
 # Create your views here.
 
 def home(request):
-    return render(request,'base.html')
-
+    return render(request,'feed.html')
+    
+def feed(request):
+    return render(request,'feed.html')
+    
 def login(request):
-    return render(request, 'login.html')
+    if request.method=="POST" :
+        username= request.POST['username']
+        password= request.POST['password']
+        # user=auth.authenticate(request, username=username, password=password)
+
+        if username is not None:
+            # auth.login(request, user)
+            return redirect('feed')
+        else:
+            return render(request, 'login.html',{'error' : 'username or password is incorrect. '})
+    else:
+        return render(request, 'login.html')
 
 def signUp(request):
     return render(request,'signUp.html')
 
-def feed(request):
-    return render(request,'feed.html')
-
 def profile(request):
     return render(request,'profile.html')
+
+def create(request):
+    new_Feed=Feed()
+    new_Feed.pub_date=timezone.datetime.now()
+    new_Feed.text=request.POST['text']
+    new_Feed.image= request.FILES['image']
+    new_Feed.author=request.user
+    new_Feed.save()
+    return redirect('home')
+
+def update(request, feed_id):
+    return redirect('home')
